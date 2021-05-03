0302b083ad9cacda9daac110ebb0c02a2a3b620e
isha2754 <42599041+isha2754@users.noreply.github.com>
2021-05-03 13:36:31 +0530

    update commit

--- DisplayAll.js
+++ DisplayAll.js
@@ -1,7 +1,4 @@
-var topicId = 12;
-
-
-
+var topicId=12;
 var i = 1;
 firebase
   .database()
@@ -11,11 +8,10 @@ firebase
     document.getElementById("questionlist").innerHTML = "";
     let co = 1;
     snapshot.forEach(function (childSnapshot) {
-      console.log(topicId);
       var total = childSnapshot.val().question;
       var questionID= childSnapshot.val().quesid;
-      let ques = `<details onclick={clickHandler(${questionID})}>
-    <summary >Question ${co} : ${total}</summary>
+      let ques = `<details onclick={clickHandler(${questionID},${total})}>
+     <summary >Question ${co} : ${total}</summary>
   </details>`;
       co++;
       var total1 = childSnapshot.val().topicId;
@@ -23,14 +19,15 @@ firebase
         $("#questionlist").append(ques);
         i++;
       }
-      console.log(total);
+
     });
   });
 
-function clickHandler(e) {
+function clickHandler(e,s) {
   //var questionss= document.getElementByClassName("question"+e.id)[0].innerHTML;
   //firebase data retreival
-
-      var url= "Answer.html?questionID=" + e;
+      console.log(e);
+      console.log(s);
+      var url= "Answer.html?questionID=" + e +"&questionName=" + s;
       window.location.href= url;
 }
